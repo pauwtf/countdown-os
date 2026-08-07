ff09a1256af5a799ff82c7edc126e027deac38e1
Paula Gutierrez <109836054+pauwtf@users.noreply.github.com>
2026-08-07 00:06:27 -0500

    Update update.py

--- python/update.py
+++ python/update.py
@@ -1,1 +1,14 @@
+from notion import get_events
 
+
+def main():
+    events = get_events()
+
+    print(f"Eventos encontrados: {len(events)}")
+
+    for event in events:
+        print(event)
+
+
+if __name__ == "__main__":
+    main()
