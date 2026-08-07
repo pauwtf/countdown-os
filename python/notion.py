407cb7edbfa8f20d7cd52dda9c00bc2a8968d7b7
Paula Gutierrez <109836054+pauwtf@users.noreply.github.com>
2026-08-07 00:59:00 -0500

    Actualizar notion.py

--- python/notion.py
+++ python/notion.py
@@ -7,22 +7,12 @@ def get_notion_client():
     return Client(auth=token)
 
 
-def test_data_sources():
+def get_events():
     notion = get_notion_client()
-    database_id = os.environ["DATABASE_ID"]
+    data_source_id = os.environ["DATA_SOURCE_ID"]
 
-    response = notion.databases.retrieve(
-        database_id=database_id
+    response = notion.data_sources.query(
+        data_source_id=data_source_id
     )
 
-    print("✅ DATABASE ENCONTRADA")
-    print("DATABASE ID:", database_id)
-
-    print("\nDATA SOURCES:")
-
-    for data_source in response.get("data_sources", []):
-        print(data_source)
-
-
-if __name__ == "__main__":
-    test_data_sources()
+    return response["results"]
